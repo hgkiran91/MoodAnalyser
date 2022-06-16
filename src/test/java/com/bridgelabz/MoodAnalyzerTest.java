d1f68df2ac6921c3bacd5233493b873908dca4ad
kiran@DESKTOP-P65JLIU <kiranmcehsn@gmail.com>
2022-06-16 13:57:36 +0530

    Added: UC1_Ability to respond happy or sad mood

--- src/test/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -4,37 +4,20 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 public class MoodAnalyzerTest {
     @Test
-    public void givenMessageWhenContainsSadItShouldReturnSadMood()throws MoodAnalyzerException {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in sad mood");
-        String actualResult = moodAnalyzer.moodAnalyzer();
+    public void givenMessageWhenContainsSadItShouldReturnSadMood() {
+        JUnitMoodAnalyser moodAnalyser = new JUnitMoodAnalyser("I am Sad");
+        String actualResult = moodAnalyser.moodAnalyser();
         String expectedMood = "sad";
-        Assertions.assertEquals(actualResult, expectedMood);
-    }
-    @Test
-    public void givenMessageWhenContainsHappyItShouldReturnHappyMood()throws MoodAnalyzerException {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in happy mood");
-        String actualResult = moodAnalyzer.moodAnalyzer();
-        String expectedMood = "happy";
-        Assertions.assertEquals(actualResult, expectedMood);
-    }
-    @Test
-    public void givenMessageWhenContainsAnyMoodItShouldReturnAnyMood()throws MoodAnalyzerException {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in any mood");
-        String actualResult = moodAnalyzer.moodAnalyzer();
-        String expectedMood = "happy";
-        Assertions.assertEquals(actualResult, expectedMood);
+        Assertions Assert = null;
+        Assert.assertEquals(actualResult, expectedMood);
     }
 
     @Test
-    public void givenMessageWhenNullItShouldReturnHappy() throws MoodAnalyzerException {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("null");
-        String actualResult;
-        try {
-            actualResult = moodAnalyzer.moodAnalyzer();
-        }
-        catch (MoodAnalyzerException e){
-            String expectedMood="happy";
-            Assertions.assertEquals(expectedMood,e.message);
-        }
+    public void givenMessageWhenContainsHappyItShouldReturnHappyMood() {
+        JUnitMoodAnalyser moodAnalyser = new JUnitMoodAnalyser("I am Happy");
+        String actualResult = moodAnalyser.moodAnalyser();
+        String expectedMood = "happy";
+        Assertions Assert = null;
+        Assert.assertEquals(actualResult, expectedMood);
     }
 }
